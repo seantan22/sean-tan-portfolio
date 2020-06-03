9b098f33e739115e2ea3ee865a9915183d359963
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-03 19:17:12 -0400

    Added headshot and bio

--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,19 @@
 import React from "react";
+import headshot from "./images/sean_headshot.JPG";
 
-export default function About({ id, subtitle }) {
+export default function About({ id }) {
   return (
-    <div className="mt-5 section">
-      <div className="section-content" id={id}>
+    <div class="mt-5 container">
+      <div id={id}>
         <h1>About</h1>
-        <p>{subtitle}</p>
+        <div class="row">
+          <div class="col-7">
+            <p class="mt-3">Hello! I'm Sean, a fourth-year student at McGill University studying software engineering and aerospace engineering.</p>
+          </div>
+          <div class="col-5">
+            <img class="img-thumbnail img-responsive" src={headshot} alt="sean_headshot" width="300" height="auto"></img>
+          </div>
+        </div>
       </div>
     </div>
   );

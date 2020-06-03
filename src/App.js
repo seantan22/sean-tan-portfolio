cbde28a027bd4483c2eb3be6b3a5522056105814
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-03 18:20:08 -0400

    Update App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import './App.css';
-import NavBar from "./navbar"
-import Intro from "./intro"
+import './App.css'
 
-function App() {
-  return (
-    <div className="container">
-      <NavBar />
-      <Intro />
-    </div> 
-  )
+import NavBar from "./components/navbar"
+import Intro from "./components/intro"
+import About from "./components/about"
+import Experience from "./components/experience"
+import Projects from "./components/projects"
+import Contact from "./components/contact"
+
+import dummyText from "./DummyText";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div class="app">
+        <div class="nav">
+          <NavBar />
+        </div>
+        <div class="body">
+          <div class="intro">
+            <Intro />
+          </div>
+          <About
+            id="about"
+            subtitle={dummyText}
+          />
+          <Experience
+            id="experience"
+            subtitle={dummyText}
+          />
+          <Projects
+            id="projects"
+            subtitle={dummyText}
+          />
+          <Contact
+            id="contact"
+          />
+          </div>
+      </div>
+    )
+  }
 }
-
-export default App;

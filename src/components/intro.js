9548ea0ceb3eb114f6682a905b57a3b28ae6b194
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-03 18:19:25 -0400

    Updated Intro

---
+++ src/components/intro.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class Intro extends React.Component {
+    render() {
+        return (
+            <div class="mt-5 pt-5">
+                <p>Hi, my name is</p>
+                <h1 class="mt-1">SEAN TAN.</h1>
+                <h2 class="mt-1">Welcome to my portfolio.</h2>
+                <h6 class="mt-5">I'm a software engineering student with a passion for developing exceptional web and mobile applications.</h6>
+                <br></br>
+                <button class="btn btn-lg btn-outline-primary mt-5">
+                    Get in Touch
+                </button>
+            </div>
+        )
+    }
+}

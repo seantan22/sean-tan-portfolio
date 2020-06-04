bb011cba4131b27f886463a004334384e22c7ae5
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-03 22:55:49 -0400

    Added social icons

---
+++ src/components/social.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Social extends React.Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <a class="fab fa-github fa-2x my-3" href="https://github.com/seantan22" />
+                    <a class="fab fa-linkedin-in fa-2x my-3" href="https://www.linkedin.com/in/seankltan/" />
+                </ul>
+            </div>
+        )
+    }
+}

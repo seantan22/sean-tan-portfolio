6919f364fb73f9033389118dec30601547097b1e
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-03 22:55:58 -0400

    Added Footer

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <div class="mt-5 pt-5">
+                <p>Designed & Built by Sean Tan © 2020</p>
+            </div>
+        )
+    }
+}

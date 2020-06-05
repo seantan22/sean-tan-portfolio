43c5e4326f21f5174f5bb6a8640d4688a125261a
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-05 01:06:23 -0400

    Edited color

--- src/components/experience.js
+++ src/components/experience.js
@@ -7,7 +7,7 @@ export default function Experience({ id }) {
       <div class="section-content" id={id}>
         <h1>Experience</h1>
         <div class="row mt-5 d-flex justify-content-center">
-          <h6>Currently available for a software development/engineering internship position from June 2020-August 2020.</h6>
+          <h6> Currently available for a software development/engineering internship position from June 2020-August 2020.</h6>
         </div>
           <SideNav />
       </div>

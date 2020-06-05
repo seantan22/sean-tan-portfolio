980b09b2f4256e0ed431ce031b3baa152e396335
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-04 23:55:59 -0400

    Added side nav

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,11 +1,15 @@
 import React from "react";
+import SideNav from "./sidenav"
 
-export default function Experience({ id, subtitle }) {
+export default function Experience({ id }) {
   return (
-    <div className="mt-5 section">
-      <div className="section-content" id={id}>
+    <div class="mt-5 section">
+      <div class="section-content" id={id}>
         <h1>Experience</h1>
-        <p>{subtitle}</p>
+        <div class="row mt-5 d-flex justify-content-center">
+          <h6>Currently available for a software development/engineering internship position from June 2020-August 2020.</h6>
+        </div>
+          <SideNav />
       </div>
     </div>
   );

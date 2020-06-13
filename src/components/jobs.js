bf2147feef6798f565e91c397d1d0db3682a5b15
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-13 00:31:26 -0400

    Renamed sidenav

--- src/components/jobs.js
+++ src/components/jobs.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default class SideNav extends React.Component {
+export default class Jobs extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -8,11 +8,12 @@ export default class SideNav extends React.Component {
             data: [
             { id: "1", job: "Snowdrop Architecture" , title: "Software Intern", year: "January 2020 - Present",  
                 desc: "Optimizing the energy usage of a sustainable tiny-home by 60% through the integration of home automation techniques. Analyzing datasets with 20000+ entries & training deep learning algorithms to recognize and predict human activity in the home.", link: "Learn More"},
-            { id: "2", job: "Blockchain at McGill" , title: "Software Design Team Member", year: "October 2019 - June 2020", 
+            { id: "2", job: "Blockchain at McGill" , title: "Software Design Team", year: "October 2019 - June 2020", 
                 desc: "Implemented a point-of-sale terminal enabling vendors to accept Ethereum or ERC-20 Tokens with a 95% success rate by    ", link: "Learn More"},
-            { id: "3", job: "Sean's Websites"  , title: "Sean", year: "March 2020 - Present", 
-                desc: "Volunteering to design and deploy multi-page websites for local businesses amid the COVID-19 pandemic to increase online traffic.", link: "Learn More"},
-            { id: "4", job: "Company"  , title: "Position", year: "Date - Date", desc: "Description."}
+            { id: "3", job: "Company"  , title: "Position", year: "Date - Date", 
+                desc: "Description.", link: "Learn More"},
+            { id: "4", job: "Company"  , title: "Position", year: "Date - Date", 
+                desc: "Description.", link: "Learn More"}
             ]
         };
         this.handleClick = this.handleClick.bind(this);

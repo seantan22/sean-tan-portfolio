2ced433ffc400077e64804275374f5fb726c6bfb
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-14 22:53:11 -0400

    New window social link

--- src/components/social.js
+++ src/components/social.js
@@ -5,10 +5,10 @@ export default class Social extends React.Component {
         return (
             <div>
                 <ul>
-                    <a href="https://github.com/seantan22">
+                    <a href="https://github.com/seantan22" target="_blank" rel="noopener noreferrer">
                         <div class="fab fa-github fa-lg mb-2"/>
                     </a>
-                    <a href="https://www.linkedin.com/in/seankltan/">
+                    <a href="https://www.linkedin.com/in/seankltan/" target="_blank" rel="noopener noreferrer">
                         <div class="fab fa-linkedin-in fa-lg my-3"/>
                     </a>
                     <div class="vl ml-2"/>

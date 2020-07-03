39a9b12391b28286330ab51a3772033fe6b16a18
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-03 14:55:25 -0400

    Styling edit

--- src/components/intro.js
+++ src/components/intro.js
@@ -6,9 +6,9 @@ export default class Intro extends React.Component {
         return (
             <div class="container intro">
                 <h4 style={{fontWeight:'lighter'}} data-aos="fade-down" data-aos-duration="3000">Hi, my name is</h4>
-                <h1 class="mt-1" data-aos="fade-down" data-aos-duration="2000">SEAN TAN.</h1>
-                <h2 class="mt-2" data-aos="fade-down" data-aos-duration="1000">Welcome to my portfolio.</h2>
-                <h4 class="mt-5" style={{fontWeight:'lighter'}} data-aos="fade-down" data-aos-duration="500">I'm a software engineering student with a passion for developing impactful applications.</h4>
+                <h1 class="mt-1" data-aos="fade-down" data-aos-duration="2000">SEAN TAN</h1>
+                <h2 class="mt-2" data-aos="fade-down" data-aos-duration="1000">Welcome to my portfolio</h2>
+                <h4 class="mt-5" style={{fontWeight:'lighter'}} data-aos="fade-down" data-aos-duration="500">I'm a software engineering student with a passion for developing impactful applications</h4>
                 <br></br>
                     <Link
                         className="btn btn-outline-dark mt-4 mx-3"

3c5899662e306900fb7314f9d64acf3d3a57b3b4
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-03 14:55:43 -0400

    Added learn more (commented out)

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,4 @@
 import React from "react";
-// import eth_pos from "./images/eth_pos.png"
-// import thebox from "./images/thebox.png"
 import cnn_asl_recog from "./images/cnn_asl_recognition_pic.png"
 import box_sim from "./images/box_sim.mp4"
 import eth_pos_demo from "./images/eth_pos_demo.mp4"
@@ -21,7 +19,6 @@ export default function Projects({ id }) {
                       <div class="fab fa-github fa-lg mb-2 ml-5"/>
                     </a>
                 </div>
-                {/* <img class="card-img-top img-fluid" src={eth_pos} alt="eth_pos" draggable="false"/> */}
                 <video class="card-img-top" loop="true" muted autoplay="autoplay">
                   <source src={eth_pos_demo} type="video/mp4"></source>
                 </video>
@@ -47,7 +44,6 @@ export default function Projects({ id }) {
                       <div class="fab fa-github fa-lg mb-2 ml-5"/>
                     </a>
                 </div>
-                {/* <img class="card-img-top" src={thebox} alt="Card cap"></img> */}
                 <video class="card-img-top" loop="true" muted autoplay="autoplay">
                   <source src={box_sim} type="video/mp4"></source>
                 </video>
@@ -82,6 +78,9 @@ export default function Projects({ id }) {
                 </div>
               </div>
             </div>
+          </div>
+          <h3 class="mt-2" data-aos="fade-right" data-aos-duration="1000">Additional Projects</h3>  
+          <div class="row">
             <div class="col-md-6 col-xl-4" data-aos="flip-right" data-aos-duration="1000">
               <div class="card bg-secondary mb-3 mt-4">
                 <div class="row d-flex justify-content-center align-items-center">
@@ -89,24 +88,27 @@ export default function Projects({ id }) {
                       <h4>Portfolio Site</h4>
                     </div>
                     <a href="https://github.com/seantan22/sean-tan-portfolio" target="_blank" rel="noopener noreferrer">
-                      <div class="fab fa-github fa-lg mb-2 ml-5"/>
+                      <div class="fab fa-github fa-lg ml-5"/>
                     </a>
                 </div>
-                {/* <img class="card-img-top" src={thebox} alt="Card cap"></img> */}
-                <div class="card-body text-dark">
-                  <p class="card-text">You're looking at it! First iteration of a single-page web application created to highlight my work and goals. Entirely designed and constructed using React. Utilized Bootstrap to ensure page responsiveness and promote smooth navigability. Hosted on GitHub Pages.</p>
-                </div>
-                <div class="card-footer d-flex justify-content-around">
-                  <small>React</small>
-                  <small>Bootstrap</small>
-                  <small>CSS</small>
-                  <small>SCSS</small>
-                  <small>Node.js</small>
-                </div>
+                {/* <img class="card-img-top" src={st_port} alt="Card cap"></img> */}
+                {/* <button class="btn btn-outline-secondary mb-2" type="button" data-toggle="collapse" data-target="#learnMorePortfolioSite" aria-expanded="false" aria-controls="learnMorePortfolioSite"><i class="fas fa-angle-double-down"></i></button> */}
+                {/* <div class="collapse multi-collapse" id="learnMorePortfolioSite"> */}
+                  <div class="card-body text-dark">
+                    <p class="card-text">You're looking at it! First iteration of a single-page web application created to highlight my work and goals. Entirely designed and constructed using React. Utilized Bootstrap to ensure page responsiveness and promote smooth navigability. Hosted on GitHub Pages.</p>
+                  </div>
+                  <div class="card-footer d-flex justify-content-around">
+                    <small>React</small>
+                    <small>Bootstrap</small>
+                    <small>CSS</small>
+                    <small>SCSS</small>
+                    <small>Node.js</small>
+                  </div>
+                {/* </div>   */}
               </div>
             </div>
             <div class="col-md-6 col-xl-4" data-aos="flip-right" data-aos-duration="1000">
-              <div class="card bg-secondary mb-3 mt-4">
+              <div class="card bg-secondary mt-4">
                 <div class="row d-flex justify-content-center align-items-center">
                     <div class="card-header d-flex justify-content-center">
                       <h4>CoffeeRun</h4>
@@ -115,16 +117,19 @@ export default function Projects({ id }) {
                       <div class="fab fa-github fa-lg mb-2 ml-5"/>
                     </a>
                 </div>
+                {/* <button class="btn btn-outline-secondary mb-2" type="button" data-toggle="collapse" data-target="#learnMoreCoffeeRun" aria-expanded="false" aria-controls="learnMorePortfolioSite"><i class="fas fa-angle-double-down"></i></button> */}
                 {/* <img class="card-img-top" src={thebox} alt="Card cap"></img> */}
-                <div class="card-body text-dark">
-                  <p class="card-text">[Coming Soon] A mobile app for simple and convenient coffee runs. Order a beverage and have it delivered directly to your seat on campus. Earn points by retrieving beverages for your study buddies.</p>
-                </div>
-                <div class="card-footer d-flex justify-content-around">
-                  <small>Java</small>
-                  <small>Spring</small>
-                  <small>Swift</small>
-                  <small>Heroku Postgres</small>
-                </div>
+                {/* <div class="collapse multi-collapse" id="learnMoreCoffeeRun"> */}
+                  <div class="card-body text-dark">
+                    <p class="card-text">[Coming Soon] A mobile app for simple and convenient coffee runs. Order a beverage and have it delivered directly to your seat on campus. Earn points by retrieving beverages for your study buddies.</p>
+                  </div>
+                  <div class="card-footer d-flex justify-content-around">
+                    <small>Java</small>
+                    <small>Spring</small>
+                    <small>Swift</small>
+                    <small>Heroku Postgres</small>
+                  </div>
+                {/* </div> */}
               </div>
             </div>
           </div>
